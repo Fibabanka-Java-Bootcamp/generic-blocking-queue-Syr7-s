2f8315c11f054b8ea772c84df4dac89f3ad29bfd
Syr7-s <isasayar7@github.com>
2020-12-27 14:00:48 +0300

    Queue and concrete queue was edited.

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -1,15 +1,38 @@
 package org.kodluyoruz;
 
-import org.kodluyoruz.abstrct.Queue;
+import org.kodluyoruz.concrete.Queue;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Merhaba Dunya");
-        Queue<Integer> integerQueue=new org.kodluyoruz.concrete.Queue<>();
-        Queue<String> stringQueue=new org.kodluyoruz.concrete.Queue<>();
+        Queue<Integer> integerQueue = new org.kodluyoruz.concrete.Queue<Integer>();
+        //Queue<String> stringQueue=new org.kodluyoruz.concrete.Queue<>();
+
         integerQueue.add(5);
+        integerQueue.add(6);
+        integerQueue.add(7);
+
+        Queue<String> stringQueue=new org.kodluyoruz.concrete.Queue<String>();
         stringQueue.add("Isa");
+        stringQueue.add("Musa");
+        stringQueue.add("Tugba");
+        stringQueue.add("Fatih");
+        stringQueue.add("Fatma");
+       /* if (stringQueue.pool()==null){
+            System.out.println("Queue empty");
+        }else{
+            System.out.println("Head removed in the queue.");
+        }*/
+        System.out.println(stringQueue.peek());
+        stringQueue.pool();
+        System.out.println(stringQueue.peek());
+        stringQueue.pool();
+        System.out.println(stringQueue.peek());
+        stringQueue.pool();
+        System.out.println(integerQueue.peek());
+
+
     }
 }
--- src/org/kodluyoruz/concrete/Queue.java
+++ src/org/kodluyoruz/concrete/Queue.java
@@ -1,18 +1,50 @@
 package org.kodluyoruz.concrete;
 
 public class Queue <T> implements org.kodluyoruz.abstrct.Queue<T> {
+    int length=0;
+    T variable;
+    Queue<T> queueHead=null;
+    Queue<T> queueTail=null;
+    Queue<T> queueNext=null;
+    Queue<T> queuePrevious=null;
+    T head=null;
+    public Queue(){}
+    public Queue(T t){
+        this.variable=  t;
+    }
     @Override
     public void add(T t) {
-        System.out.println("add " +t);
+        Queue queue=new Queue(t);
+        if (this.queueHead==null){
+            this.queueHead=queue;
+            this.queueTail=queue;
+            this.length=1;
+        }else{
+            //queue.queueNext=this.queueHead;
+            queue.queueNext=this.queueTail;
+            this.queuePrevious=queue;
+            this.queueTail=queue;
+            ++this.length;
+        }
+        System.out.println("queueHead "+queueHead.variable+" queueTail "+queueTail.variable);
     }
 
     @Override
     public T pool() {
-        return null;
+        if (queueHead==null){
+            System.out.println("No items queue");
+        }else{
+            System.out.println("********************");
+            head= queueHead.variable;
+            queueHead=queueTail.queueNext;
+            System.out.println("Head: "+head+" queue Head: "+queueHead.variable+" queue Tail: "+queueTail.variable);
+        }
+        return head;
     }
 
     @Override
     public T peek() {
-        return null;
+
+        return queueHead.variable;
     }
 }
